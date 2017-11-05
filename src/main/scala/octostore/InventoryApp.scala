d2dccc14c556628f9617c395f4ba39504022641b
k0ner <konradtendera@gmail.com>
2017-11-04 23:21:44 -0500

    Replace try-finally with Try

--- src/main/scala/octostore/InventoryApp.scala
+++ src/main/scala/octostore/InventoryApp.scala
@@ -3,17 +3,19 @@ package octostore
 import akka.actor.ActorSystem
 
 import scala.io.StdIn
+import scala.util.Try
 
 object InventoryApp extends App {
 
   val system = ActorSystem("inventory-system")
 
-  try {
-    system.actorOf(InventorySupervisor.props(), "inventory-supervisor")
+  Try(startInventorySystem)
+  system.terminate()
+
+  def startInventorySystem = {
     // Create top level supervisor
+    system.actorOf(InventorySupervisor.props(), "inventory-supervisor")
     StdIn.readLine()
-  } finally {
-    system.terminate()
   }
 
 }
