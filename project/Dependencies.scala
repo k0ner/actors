640b74ca912523d50575f53abd06a1756295bd74
k0ner <konradtendera@gmail.com>
2017-11-15 19:59:54 -0600

    Test HealthCheckRouting directive

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,25 +3,27 @@ import sbt._
 object Dependencies {
 
   val AkkaVersion = "2.5.6"
+  val AkkaHttpVersion = "10.0.10"
   val Json4sVersion = "3.5.3"
+  val ScalaTestVersion = "3.0.4"
 
   val engineDependencies: Seq[ModuleID] = Seq(
     "com.typesafe.akka" %% "akka-actor" % AkkaVersion,
     "org.scalactic" %% "scalactic" % "3.0.4",
-    "org.scalatest" %% "scalatest" % "3.0.4" % "test",
+    "org.scalatest" %% "scalatest" % ScalaTestVersion % "test",
     "com.typesafe.akka" %% "akka-testkit" % AkkaVersion % "test",
     "com.gilt" %% "gfc-timeuuid" % "0.0.8" // is it needed ??
   )
 
   val restDependencies: Seq[ModuleID] = Seq(
-    "com.typesafe.akka" %% "akka-http" % "10.0.10",
+    "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
     "com.typesafe.akka" %% "akka-actor" % AkkaVersion,
     "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
-    "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.10",
-    "de.heikoseeberger" % "akka-http-json4s_2.12" % "1.18.1",
+    "de.heikoseeberger" %% "akka-http-json4s" % "1.18.1",
     "org.json4s" %% "json4s-native" % Json4sVersion,
     "org.json4s" %% "json4s-ext" % Json4sVersion,
-    "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+    "com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion,
+    "org.scalatest" %% "scalatest" % ScalaTestVersion % "test"
   )
 
 }
